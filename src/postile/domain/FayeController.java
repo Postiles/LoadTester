30a07f49f92bce3b1e5bad8c5d7fbb88b80890d8
Gary LI <litao912002@gmail.com>
2013-03-29 14:37:36 -0500

    Hand shake with faye implemented

--- src/postile/domain/FayeController.java
+++ src/postile/domain/FayeController.java
@@ -4,6 +4,11 @@
  */
 package postile.domain;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.List;
 import postile.test.controller.Tester;
 import postile.test.netutil.HttpRequester;
 
@@ -13,12 +18,40 @@ import postile.test.netutil.HttpRequester;
  */
 public class FayeController {
 	int clientId;
-	public static String HSHAKE_PATH="/faye/meta/handshake";
-	public static String FayeURL="http://www.postiles.com:9292";
-	public void handshake() {
-		String url = FayeURL+HSHAKE_PATH;
-		String rJson = HttpRequester.executePostFaye(url,"{\"channel\":\"/meta/handshake\",\"supportedConnectionTypes\":[\"long-polling\"],\"version\":\"1.0\"}");
+	public static String FayeURL="http://www.postiles.com:9292/faye";
+	public static final String FayeQueryFormat = "{\"channel\":\"%s\",\"%s\"}";
+	public static final String HandShakeChannel = "/meta/handshake";
+	public static final String FayeHandShakeQuery="supportedConnectionTypes\":"
+			+ "[\"long-polling\"],\"version\":\"1.0\"";
+	HandShakeResponse fayeClient;
+	public void handshake(){
+		String url = FayeURL;
+		String query = String.format(FayeQueryFormat,HandShakeChannel,FayeHandShakeQuery);
+		String rJson = HttpRequester.executePostFaye(url,query);
 		System.out.printf("%s\n", rJson);
+		Gson gson = new Gson();
+		List<HandShakeResponse> responses = gson.fromJson(rJson, 
+				new TypeToken<List<HandShakeResponse>>(){}.getType());
+		fayeClient = responses.get(0);
+		System.out.printf("Hand Shake success: Client ID:%s\n", 
+				fayeClient.clientId);
 	}
-	
+	public void subscribe() {
+	}
+}
+
+
+class HandShakeResponse {
+	public String channel;
+	public boolean successful;
+	public String version;
+	public List<String> supportedConnectionTypes;
+	public String clientId;
+	public FayeAdvice advice;
+}
+
+class FayeAdvice {
+	public String reconnect;
+	public int interval;
+	public int timeout;
 }
